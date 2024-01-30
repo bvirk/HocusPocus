99fccf00ba90aeed5b9a9a4d5e25477337d902a5
Benny Andersen <a.mail.user@gmail.com>
2024-01-30 23:52:22 +0100

    experimenting

--- domain/data/progs/plain/index.php
+++ domain/data/progs/plain/index.php
@@ -1,3 +1,21 @@
 <?php
-$content = 'plain ok';
-return $content;
+$dir = 'data/snut/demo';
+
+//$b = chmod("config/filetoedit.txt",0111);
+$stat = mkdir($dir);
+/* copy ErrorException
+access to dest
+missing source
+mising dir
+OK - der kastes
+*/
+/* chmod ErrorException
+no such file
+permission
+*/
+/* mkdir exception
+no path
+permission
+*/
+
+return "stat=".($stat ? "true" : "false");
