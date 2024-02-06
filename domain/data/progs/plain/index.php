fd4679aba46d5e364a01b46cbaaecb3f8b4c348a
Benny Andersen <a.mail.user@gmail.com>
2024-02-06 16:57:13 +0100

    test

--- domain/data/progs/plain/index.php
+++ domain/data/progs/plain/index.php
@@ -1,21 +1,8 @@
 <?php
-$dir = 'data/snut/demo';
 
-//$b = chmod("config/filetoedit.txt",0111);
-$stat = mkdir($dir);
-/* copy ErrorException
-access to dest
-missing source
-mising dir
-OK - der kastes
-*/
-/* chmod ErrorException
-no such file
-permission
-*/
-/* mkdir exception
-no path
-permission
-*/
+use function actors\isLoggedIn;
 
-return "stat=".($stat ? "true" : "false");
+
+
+//return (isLoggedIn() ? "true ":"false " ).'logout '.$_SESSION['loggedin'] != USERS[0] ? $_SESSION['loggedin']:'';
+return "done";
