0c39b6298d289559bafd9fc2ceeb4b8de071ff09
Benny Andersen <a.mail.user@gmail.com>
2024-02-10 22:06:25 +0100

    restructured

--- domain/progs/NNNAPI.php
+++ domain/progs/NNNAPI.php
@@ -4,6 +4,14 @@ namespace progs;
 
 use function actors\datafileExists;
 
+function  atTopDir() {
+    if ($_GET['curdir'] === 'pages') {
+        echo [CONFIRM_COMMAND,'toplevel directories does not apply'];
+        return true;
+    }
+    return false;
+}
+
 function catcher($f) {
     try {
         $fn = __NAMESPACE__."\\".$f;
@@ -19,9 +27,7 @@ function changeNameSpace($oldBasename,$newBasename,$path) {
         $all .= preg_match('/^namespace /',$line)
             ? preg_replace("/(?<=\\\\)$oldBasename/",$newBasename,$line)
             : $line;
-    $numSaved = file_put_contents($path,$all);
-    return $numSaved;
-
+    file_put_contents($path,$all);
 }
 
 function changeNamespaceInDirs(string $old,string $new,string $path,$relPos=0) { 
@@ -52,10 +58,67 @@ function copySubDirsOf($file,$rootLen=0) {
             copySubDirsOf("$file/$fileInDir",$rootLen);
 }
 
-function hasSubDir($dir) {
-    $globPatt=rtrim($dir,'/').'/*';
-    $filesArr = glob($globPatt,GLOB_ONLYDIR);
-    return count($filesArr) > 0; 
+function hasReadAccess($path) {
+    $owner = posix_getgrgid(filegroup($path))['name'];
+    $readFlag =  fileperms($path) & 040;
+    $readFlagDir = is_dir($path) ? $readFlag  : fileperms(dirname($path)) & 040;
+    return hasReadAccessFor($owner,$readFlag,$readFlagDir);
+}
+
+function hasReadAccessFor($owner,$readFlag,$readFlagDir) {
+    return $_SESSION[LOGGEDIN] == APACHE_USER || $owner == $_SESSION[LOGGEDIN] || ($readFlag & $readFlagDir);
+}
+
+function getOwner($path) {
+    $owner = posix_getgrgid(filegroup($path))['name'];
+    $readFlag =  fileperms($path) & 040;
+    $readFlagDir = is_dir($path) ? $readFlag  : fileperms(dirname($path)) & 040;
+    return [$owner,$readFlag,$readFlagDir];
+}
+
+
+function hasWriteAccess($path) {
+    $group = posix_getgrgid(filegroup($path))['name'];
+    if ( $group == APACHE_USER || $group == $_SESSION[LOGGEDIN])
+        return true;
+    echo json_encode([CONFIRM_COMMAND, $_SESSION[LOGGEDIN].' is not owner of '.$_GET['selname'] ]);
+    return false;
+            
+}
+
+function loggedInIsApache() {
+    if (APACHE_USER !== $_SESSION[LOGGEDIN]) {
+        echo json_encode([CONFIRM_COMMAND,"only for ".APACHE_USER]);
+        return false;
+    }
+    return true;
+}
+
+function isAUser($user) {
+    if (!in_array($user,USERS)) {
+        echo json_encode([CONFIRM_COMMAND,"user $user don't exists"]);
+        return false;
+    }
+    return true;
+}
+
+function isClassDir($classPath) {
+    if (file_exists($classPath)) {
+        echo json_encode([CONFIRM_COMMAND, "$classPath has subdirs - delete them first"]);
+        return true;
+    }
+    return false;
+}
+
+function isLegaldataFileName() {
+    extract(pathinfo($_GET['txtinput']),EXTR_PREFIX_ALL,"txtinput");
+    $txtinput_ext = $txtinput_extension ?? '';
+                
+    if ( ($txtinput_ext !=='md' && $txtinput_ext !== 'php') || strpos($txtinput_filename,'.') !== false) {
+        echo json_encode([CONFIRM_COMMAND,"file must have extension 'md' or 'php' and not contain other dots"]);
+        return false;
+    }
+    return true;
 }
 
 function indexIsLinkOf($file) {
@@ -66,9 +129,18 @@ function indexIsLinkOf($file) {
     return readlink($index) == basename($file);
 }
 
+function selectedIsIndex() {
+    if (pathinfo($_GET['selname'],PATHINFO_FILENAME) == 'index') {
+        echo json_encode([CONFIRM_COMMAND,'index does not apply']);     
+        return true;
+    }
+    return false;
+}
+
 function lgdIn_copy($from,$dest) {
     copy($from,$dest);
     chgrp($dest,$_SESSION[LOGGEDIN]);
+    chmod($dest,0666);
 }
 
 function lgdIn_mkdir($dir) {
@@ -91,50 +163,41 @@ function lgdIn_rename($from,$dest) {
     chgrp($dest,$_SESSION[LOGGEDIN]);
 }
 
-/**
- * Make datadir
- * @return array [0,'ok']
- */
-function mkDDir():array {
-    $file = $_GET['curdir'].'/'.$_GET['txtinput'];
- 
-    if (array_key_exists('extension',pathinfo($file)) )
-        return [CONFIRM_COMMAND,'extension not allowed'];
-    if ($_GET['curdir'] === 'pages' )
-        return [CONFIRM_COMMAND,'dir must be below pages'];
-    mkdir("data/$file");
-    copy('config/datafile',"data/$file/index.md");
-    chmod("data/$file/index.md",0666);
-    newClass($file);
-    return [REDRAW_DIR,''];
-}
 
-function newDFile() {
-    $file = $_GET['curdir'].'/'.$_GET['txtinput'];
-    $ext = pathinfo($file)['extension'] ?? '';
-    if ($_GET['curdir'] === 'pages')
-        return [CONFIRM_COMMAND,'file must reside below pages'];
-    if ( $ext !=='md' && $ext !== 'php')
-        return [CONFIRM_COMMAND,"file must have extension 'md' or 'php'"];
-    copy('config/datafile',"data/$file");
-    chmod("data/$file",0606);
-    chgrp("data/$file",$_SESSION[LOGGEDIN]);
-    return [REDRAW_DIR,''];
+/**
+ * Rename including make filename index, when being a link that points to the original file or directory, 
+ * a link the point to the new file or directory.
+ * 
+ * @param string $orgPath is the file or dir to be renamed
+ * @param string $newFileWOE is filename without path or extension or name or dir without path.
+ * @param bool $followExtension when true, as it deafult to, searches for possible existing link named index with same extension as  original file, when false the possible existing with extension .md or .php. 
+ */
+function mvInclLink(string $orgPath,string $newFileWOE,bool $followExtension=true) : void {
+    extract(pathinfo($orgPath),EXTR_PREFIX_ALL,"org");
+    $org_ext = isset($org_extension) ? ".$org_extension" :''; 
+    $index = "$org_dirname/index";
+    if ($followExtension) // both dir and file
+        $index .=  $org_ext;
+    else  // for datafile - index.md or index.php
+        \actors\datafileExists($index);
+    $newPath = "$org_dirname/$newFileWOE$org_ext";    
+    if (is_link($index)) {
+        unlink($index);
+        \actors\lnRel($newPath,$index);
+    }
+    lgdIn_rename($orgPath,$newPath);
 }
 
 function newClass($file) {
     $dirName = dirname($file);
     $baseName= basename($file);
     $classPath = "$dirName/".ucfirst($baseName).'.php';
-    $retval=true;
-    if (!file_exists($dirName)) {
-        $retval = mkdir($dirName,0777);
-    }
-    return $retval &&
-        file_put_contents($classPath,
-        "<?php\nnamespace ".str_replace('/','\\',$dirName). ";\nclass ".ucfirst($baseName)." extends \\actors\\StdMenu {\n}\n")
-        &&
-        chmod($classPath,0666);
+    if (!file_exists($dirName)) 
+        mkdir($dirName,0777);
+    file_put_contents($classPath,
+        "<?php\nnamespace ".str_replace('/','\\',$dirName)
+        . ";\nclass ".ucfirst($baseName)." extends \\actors\\StdMenu {\n}\n");
+    chmod($classPath,0666);
 }
 
 function removeBesidesRoot($str,$removeThisDir=false) {
@@ -153,13 +216,9 @@ function renameClass(string $old,string $new,string $dataPath) {
     [$OldFile,$NewFile] = [DOC_ROOT."/$dataPath/$Old.php",DOC_ROOT."/$dataPath/$New.php"];
     
     $content=preg_replace('/\nclass\s+'.$Old.'/',"\nclass $New",file_get_contents($OldFile));
-    $retval = file_put_contents($NewFile,$content);
-    if ($retval) {
-        unlink($OldFile);
-        chmod($NewFile,0666);
-    }
-    return $retval;    //return false;
-
+    file_put_contents($NewFile,$content);
+    unlink($OldFile);
+    chmod($NewFile,0666);
 }
 
 function renameExterns($path,$from,$to) {
@@ -170,15 +229,24 @@ function renameExterns($path,$from,$to) {
           
        foreach ($extArr as $ext) {
            $srcPath = "$docRoot/".substr($path,strlen(DEFCONTENT)+1);
-           renameOnExists("$srcPath/$from.$ext","$srcPath/$to.$ext");
+           renameOnExists("$srcPath/$from.$ext",$to);
        }
    }
 }
 
-function renameOnExists(string $from,string $to,bool $ignoreExisting=true) {
-    if (file_exists($from)) 
-        return rename($from,$to);
-    return $ignoreExisting;
+function renameOnExists(string $pathFrom,string $fileWOETo) {
+    if (file_exists($pathFrom)) { 
+        mvInclLink($pathFrom,$fileWOETo);
+    }
+
+}
+
+function txtinputContainsDot() {
+    if (array_key_exists('extension',pathinfo($_GET['txtinput']))) {
+        echo [CONFIRM_COMMAND,'dots in txtinput not allowed here'];
+        return true;
+    }
+    return false;
 }
 
 function toTrash($file) {
@@ -201,14 +269,19 @@ function toTrash($file) {
 }
 
 function trashDir($classPath) {
-    foreach (glob("data/$classPath/*") as $p)
-        trashReferedByDataFile(dirname($p),basename($p));
+    $index = "data/$classPath/index";
+    datafileExists($index);
+    trashReferedByDataFile("data/$classPath",basename($index));
+    foreach (nodotScandir("data/$classPath") as $p)
+        if ($p !== 'index.md' && $p !== 'index.php' )
+            trashReferedByDataFile("data/$classPath",$p);
     rmdir("data/$classPath");
     // trash class
     toTrash(dirname($classPath).'/'.ucfirst(basename($classPath)).'.php');
     // rm dir of classes if previos was last
-    if (count(glob(dirname($classPath.'/*')))==0) 
-    rmdir(dirname($classPath));
+    $otherClassFiles = glob(dirname($classPath).'/*');
+    if (count($otherClassFiles)== 0) 
+        rmdir(dirname($classPath));
 }
 
 
@@ -241,16 +314,6 @@ function trashSpecialExterns($path,$file) {
                 toTrash($fileToTrash);
        }
    }
-}
-
-function tooglePublic() {
-    $file = $_GET['file'];
-    $gname = posix_getgrgid(filegroup($file))['name'];
-    if ($gname !== $_SESSION[LOGGEDIN])
-        return [IS_PHP_ERR,'You are not owner of '.basename($file)];
-    return chmod($file,fileperms($file)^0x20)
-        ? [REDRAW_DIR,'']
-        : [CONFIRM_COMMAND,'unable to change group read flag'];
 }
 
 function ucAfterLast($text,$search='/') {
@@ -287,6 +350,23 @@ class NNNAPI {
         headerCTText();
         $usesJSON=true;
     }
+
+    function chmod() {
+        if (!loggedInIsApache())
+            return;
+        $selnameDataPath= 'data/'.$_GET['curdir'].'/'.$_GET['selname'];
+        $mode = intval($_GET['txtinput'],8);
+        chmod($selnameDataPath,$mode);
+        echo json_encode([REDRAW_DIR,'']);
+    }
+    
+    function chown() {
+        if (!loggedInIsApache() || !isAUser($_GET['txtinput']))
+            return;
+        $selnameDataPath= 'data/'.$_GET['curdir'].'/'.$_GET['selname'];
+        $result = chgrp($selnameDataPath,$_GET['txtinput']);
+        echo json_encode([REDRAW_DIR,'']);
+    }
     
     function edit() {
         $fileToEdit  = $_GET['filetoedit'];
@@ -305,12 +385,20 @@ class NNNAPI {
         $dirList = [];
         $dir = DATA_ROOT.'/'.$_GET['curdir'];
         foreach (nodotScandir($dir) as $file) {
+            $owner = posix_getgrgid(filegroup("$dir/$file"))['name'];
+            $readFlag =  fileperms("$dir/$file") & 040;
+            if (! hasReadAccessFor($owner,$readFlag,$readFlag))
+                continue;
             $fileIsDir = is_dir("$dir/$file");
+            $liClass = $fileIsDir ? '/Dir' : ' File';
+            if ($owner == $_SESSION[LOGGEDIN])
+                $liClass .= $readFlag ? 'OwnerRead' : 'OwnerNotRead';
+            
             // the class that implement the files of dir or the file 
             $implClass = $fileIsDir ? ($_GET['curdir'].'/'.ucfirst("$file.php")) : ucAfterLast($_GET['curdir']).'.php';
             $dirList[] = [
                 $file
-                ,($fileIsDir ? '/' : '')
+                ,$liClass
                 ,\actors\filespec("$dir/$file")
                 ,\actors\enheritChain($implClass)];
             
@@ -319,80 +407,89 @@ class NNNAPI {
     }
     
     function mkDir() {
-        echo json_encode(mkDDir());
+        $txtinputPath = $_GET['curdir'].'/'.$_GET['txtinput'];
+        $txtinputDataPath = 'data/'.$_GET['curdir'].'/'.$_GET['txtinput'];
+ 
+        if (txtinputContainsDot() || atTopDir())
+            return;
+        lgdIn_mkdir($txtinputDataPath);
+        lgdIn_copy('config/datafile',"$txtinputDataPath/index.md");
+        newClass($txtinputPath);
+        echo [REDRAW_DIR,''];
     }
 
     function mv() {
-        $srcWOE=preg_replace('/\|\w+$/','',$_GET['selname']);
-        $destWOE=preg_replace('/\|\w+$/','',$_GET['txtinput']);
-        $imgPathPath = IMG_ROOT.'/'.$_GET['curdir'];
-        $imgSrcDir = "$imgPathPath/$srcWOE";
-        $imgDestDir ="$imgPathPath/$destWOE";
-        renameOnExists($imgSrcDir,$imgDestDir);
-        renameExterns($_GET['curdir'],$srcWOE,$destWOE);
-        $dir = DATA_ROOT.'/'.$_GET['curdir'];
-        [$srcFile,$destFile] = [
-            "$dir/".$_GET['selname']
-            ,"$dir/".$_GET['txtinput']];
-        echo json_encode([renameOnExists($srcFile,$destFile,false) 
-            ? [REDRAW_DIR,''] 
-            : CONFIRM_COMMAND,"$srcFile did not exist"]);
+        $selname_filename=pathinfo($_GET['selname'],PATHINFO_FILENAME);
+        extract(pathinfo($_GET['txtinput']),EXTR_PREFIX_ALL,"txtinput");
+        $txtinput_ext = $txtinput_extension ?? '';
+        $imgSelPath = 'img/'.$_GET['curdir']."/$selname_filename";
+        $selDataPath = 'data/'.$_GET['curdir'].'/'.$_GET['selname'];
+ 
+        if (!hasWriteAccess($selDataPath) || selectedIsIndex() || !isLegaldataFileName()) 
+            return;
+        
+        renameOnExists($imgSelPath,$txtinput_filename);
+        renameExterns($_GET['curdir'],$selname_filename,$txtinput_filename);
+        mvInclLink($selDataPath,$txtinput_filename,false);
+        echo json_encode([REDRAW_DIR,'']); 
     }
 
+    
+    // Not Testet from here
+    
     function mvDir()  {   
-        $from = $_GET['selname'];
-        $to = $_GET['txtinput'];
-        $path = $_GET['curdir'];
-        rename(DATA_ROOT."/$path/$from",DATA_ROOT."/$path/$to");
-        renameClass($from,$to,$path);
-        if (file_exists(/* as dir in pages/ */ DOC_ROOT."/$path/$from")) { // it would flag for inconsistence otherwise
-            rename(DOC_ROOT."/$path/$from",/* new name of that dir */ DOC_ROOT."/$path/$to");
-            changeNamespaceInDirs($from,$to,DOC_ROOT."/$path/$to");
+        $selPath = $_GET['curdir'].'/'.$_GET['selname'];
+        $selDataPath = "data/$selPath";
+        $txtinputPath = $_GET['curdir'].'/'.$_GET['txtinput'];
+        $txtinputDataPath = "data/$txtinputPath";
+        $imgSelPath = 'img/'.$_GET['curdir'].'/'.$_GET['selname'];
+        $imgTxtinputPath = 'img/'.$_GET['curdir'].'/'.$_GET[' txtinput'];
+        
+        if (!hasWriteAccess($selDataPath) || txtinputContainsDot() || atTopDir()) 
+            return;
+        lgdIn_rename($selDataPath,$txtinputDataPath);
+        renameClass($_GET['selname'],$_GET['txtinput'],$_GET['curdir']);
+        if (file_exists(/* as dir in pages/ */ $selPath  )) { 
+            rename("$selPath",/* new name of that dir */ $txtinputPath);
+            changeNamespaceInDirs($_GET['selname'],$_GET['txtinput'],$txtinputPath);
         }
-        $imgPathPath = IMG_ROOT.'/'.$_GET['curdir'];
-        renameOnExists("$imgPathPath/$from","$imgPathPath/$to");
-        renameExterns($_GET['curdir'],$from,$to);
+        renameOnExists($imgSelPath,$imgTxtinputPath);
+        renameExterns($_GET['curdir'],$_GET['selname'],$_GET[' txtinput']);
         echo [REDRAW_DIR,''];
     }
 
     function newFile() {
-        echo json_encode(newDFile());
+            $txtinputDataPath = $_GET['curdir'].'/'.$_GET['txtinput'];
+            
+            if (atTopDir() || !isLegaldataFileName())
+                return;
+            lgdIn_copy('config/datafile',$txtinputDataPath);
+            echo [REDRAW_DIR,''];
     }
 
     function rm() {
-        $selname = $_GET['selname'];
-        $classPath = $_GET['curdir'];
-        $group = posix_getgrgid(filegroup("data/$classPath/$selname"))['name'];
-         
-        if ( $group !== APACHE_USER && $group !== $_SESSION[LOGGEDIN]) {
-            echo  json_encode([CONFIRM_COMMAND,$_SESSION[LOGGEDIN]." is not user of $selname"]);
+        $selDataPath = 'data/'.$_GET['curdir'].'/'.$_GET['selname'];
+        if (!hasWriteAccess($selDataPath))
             return;
+ 
+        if ($_GET['selname'] !== 'index.md' && $_GET['selname'] !== 'index.php' && !indexIsLinkOf($selDataPath)) 
+            trashReferedByDataFile('data/'.$_GET['curdir'],$_GET['selname']);
+        else {
+            if (isClassDir($_GET['curdir']))
+                return;
+            trashDir($_GET['curdir']);
         }
-        if ($selname !== 'index.md' && $selname !== 'index.php' && !indexIsLinkOf("data/$classPath/$selname")) {
-            trashReferedByDataFile("data/$classPath",$selname);
-            echo json_encode([REDRAW_DIR,'']);
-            //echo json_encode([CONFIRM_COMMAND,'remove file '.$_GET['selname']]);
-        } else {
-            if (hasSubDir("data/$classPath"))
-                echo json_encode([CONFIRM_COMMAND,"$classPath has subdirs - delete them first"]);
-            else {
-                trashDir($classPath);
-                //echo json_encode([CONFIRM_COMMAND,'trashdir '.$classPath]);
-                echo json_encode([REDRAW_UPPERDIR,'']);
-            }
-        }
+        echo json_encode([REDRAW_UPPERDIR,'']);
     }
 
     function rmDir() {
-        $dirAsSelname = $_GET['selname'];
-        $classPath = $_GET['curdir'];
-        
-        if (hasSubDir("data/$classPath/$dirAsSelname"))
-            echo json_encode([CONFIRM_COMMAND,"$classPath/$dirAsSelname has subdirs - delete them first"]);
-        else {
-            trashDir("$classPath/$dirAsSelname");
-            echo json_encode([REDRAW_UPPERDIR,'']);
-        }
+        $selPath = $_GET['curdir'].'/'.$_GET['selname'];
+        $selDataPath = 'data/'.$selPath;
+ 
+        if (!hasWriteAccess($selDataPath) || isClassDir($_GET['curdir']))
+                return;
+        trashDir($selPath);
+        echo json_encode([count(glob($_GET['curdir'].'/*')) ? REDRAW_DIR : REDRAW_UPPERDIR,'']);
     } 
 
     function saveFile() {
@@ -414,22 +511,23 @@ class NNNAPI {
         echo $value;
     }
 
-    function test() {
-        $index= 'data/'.$_GET['curdir'].'/'.$_GET['file'];
-        
-        
-        echo json_encode([CONFIRM_COMMAND,indexIsLinkOf('data/'.$_GET['curdir'].'/'.$_GET['file'])
-            ? 'index points at selected' 
-            : "index do not point at selected"]);
-    }
 
     function tooglePublic() {
-        echo json_encode(tooglePublic());
+        $selDataPathDir='data/'.$_GET['curdir'];
+        $selDataPath = $selDataPathDir.'/'.$_GET['selname'];
+        
+        if (!hasWriteAccess($selDataPath))
+            return;
+        $GR_xorBit = (fileperms($selDataPath) & 040)^040;
+        chmod($selDataPath,(fileperms($selDataPath) & 0737) | $GR_xorBit);
+        if (pathinfo($_GET['selname'],PATHINFO_FILENAME) == 'index')
+            chmod($selDataPathDir,(fileperms($selDataPathDir) & 0737) | $GR_xorBit);
+        echo json_encode([REDRAW_DIR,'']);
     }
 
     function undoTrash() {
-        $mes = copySubDirsOf('trash/'.$_SESSION[LOGGEDIN]) ? "trash restored - 't' for empty it" : 'some fail in trash restoring'; 
-        echo json_encode([CONFIRM_COMMAND,$mes]);
+        copySubDirsOf('trash/'.$_SESSION[LOGGEDIN]);
+        echo json_encode([CONFIRM_COMMAND,'trash/'.$_SESSION[LOGGEDIN]." restored - use 't' for emptying"]);
     }
 
 }
