c240c23f0ad0fb9fc5a957d7107c6c4c8dd0cd2f
Benny Andersen <a.mail.user@gmail.com>
2024-02-15 16:11:02 +0100

    shortend on cleverified by moving pageExterns to Pagefuncs.php

--- domain/data/progs/html/extern.php
+++ domain/data/progs/html/extern.php
@@ -1,47 +1,23 @@
 <?php
 if (!array_key_exists('refer',$_GET))
     return "#### no refer path given";
-function docfile_exists($fileName) {
-    return file_exists(DOC_ROOT."/$fileName");
-}
-function refs($wayPath) {
-    $wayRefs = '';
-    foreach (['css' => ['css'],'js' => ['js','php']] as $droot => $extArr)
-        foreach($extArr as $ext) {
-            $drootFilename = "$droot/$wayPath.$ext";
-            $link = $ext == 'php' ? "/progs/html/source/file=$droot/$wayPath|$ext" : "/$droot/$wayPath.$ext";
-            $wayRefs .= file_exists($drootFilename) 
-                    ? "[$droot/$wayPath.$ext]($link)  \n" 
-                    : "$droot/$wayPath.$ext  \n";
-        }
-    return $wayRefs;
-}
-
-$path = $_GET['refer'];
-$ppe = explode('/',$path);
-$pfunc = array_slice($ppe,-1,1)[0];
-$pUrlDir=implode('/',array_slice($ppe,1,-1));
-$pclassPath = implode('/',array_slice($ppe,0,-2)).'/'.ucfirst(array_slice($ppe,-2,1)[0]).'.php';
-if (!docfile_exists($pclassPath))
-    return "#### Url $path dont address a page";
 
 $externsStr='';
-foreach ([
-     '#### page specials' => ["$pUrlDir/$pfunc"]
-    ,'#### url hierarchy' => array_slice($ppe,1,-1)
-    ,'#### class hierarchy' => explode("\\",\actors\enheritChain($pclassPath))] as $eType => $eTypeArr) {
-    [$urltype,$acum,$slash]=[[],'',''];        
-    foreach ($eTypeArr as $cpe) {
-        $acum .= "$slash$cpe";
-        $urltype[] = refs($acum);
-        $slash='/';
+$path = $_GET['refer'];
+foreach(['css','js'] as $ext) {
+    $externsStr .= "#### $ext hierarchy\n|filename|file info|type|\n|:--|:--|:--|\n";
+    foreach (actors\pageExternsOfType($ext,$_GET['refer']) as $line ) {
+        $externsStr .= $line[1] == 'e'
+            ? "|[$line[0]](/?path=progs/html/source&file=$line[0])|$line[2]|"
+            : "|$line[0]| $line[2]|"; 
+        $externsStr .= "$line[3]|\n";
     }
-    $externsStr .= "$eType\n".implode("\n",$urltype)."\n";
-
 }
+
 return [<<< EOMD
 ## Extern references of $path
 $externsStr
 #### Image path  
 img/$path
+
 EOMD];
