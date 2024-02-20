7f5acb384cae8fe87f05556049e99f81428c70e2
Benny Andersen <a.mail.user@gmail.com>
2024-02-20 23:49:47 +0100

    minor spell fix

--- domain/data/progs/html/extern.php
+++ domain/data/progs/html/extern.php
@@ -4,8 +4,10 @@ if (!array_key_exists('refer',$_GET))
 
 $externsStr='';
 $path = $_GET['refer'];
+$tblHead="|filename|file info|type|\n|:--|:--|:--|\n";
+
 foreach(['css','js'] as $ext) {
-    $externsStr .= "#### $ext hierarchy\n|filename|file info|type|\n|:--|:--|:--|\n";
+    $externsStr .= "#### $ext hierarchy\n$tblHead";
     foreach (actors\pageExternsOfType($ext,$_GET['refer']) as $line ) {
         $externsStr .= $line[1] == 'e'
             ? "|[$line[0]](/?path=progs/html/source&file=$line[0])|$line[2]|"
@@ -14,10 +16,13 @@ foreach(['css','js'] as $ext) {
     }
 }
 
-return [<<< EOMD
+$externsStr .= "#### Images\n$tblHead";
+$iList=actors\pageImages($path);
+foreach($iList as $line) 
+    $externsStr .= "|$line[0]|$line[2]|$line[3]|\n";
+
+    return [<<< EOMD
 ## Extern references of $path
 $externsStr
-#### Image path  
-img/$path
 
 EOMD];
