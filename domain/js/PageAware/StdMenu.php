7dcc7fbc02bb2639e5d2a82b7a20dae8c6e3c91e
Benny Andersen <a.mail.user@gmail.com>
2024-02-27 21:03:30 +0100

    simplified

--- domain/js/PageAware/StdMenu.php
+++ domain/js/PageAware/StdMenu.php
@@ -5,7 +5,7 @@ global $pe; ?>
     const defaultPage=<?="'".defaultPage()."';\n"?>
     const isLoggedin=<?=(in_array($_SESSION[LOGGEDIN],USERS)? 'true' : 'false').";\n"?>
     const isPHPErr=<?="'".IS_PHP_ERR."';\n"?>
-    const loggedInIsApache=<?="'".($_SESSION[LOGGEDIN] == APACHE_USER ? 'true' : 'false')."';\n"?>
+    const loggedInIsApache=<?=($_SESSION[LOGGEDIN] == APACHE_USER ? 'true' : 'false').";\n"?>
     const redrawDir=<?="'".REDRAW_DIR."';\n"?>
     const redrawUpperDir=<?="'".REDRAW_UPPERDIR."';\n"?>
 <?php if (implode('/',$pe) != 'progs/edit/content'): ?>
