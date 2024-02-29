bb256c9b82144bbd2a2779916d7ac66f58a37413
Benny Andersen <a.mail.user@gmail.com>
2024-02-29 23:08:16 +0100

    removed old document root constants

--- domain/defines.php
+++ domain/defines.php
@@ -1,24 +1,17 @@
 <?php
 define("APACHE_USER",exec('whoami'));
 define("AUTHFILE","config/encrypted.php");
-define("CSS_ROOT",$_SERVER['DOCUMENT_ROOT'].'/css');
 define("CONFIRM_COMMAND",'errOrConf');
-define("DATA_ROOT",$_SERVER['DOCUMENT_ROOT'].'/data');
-define("DOC_ROOT",$_SERVER['DOCUMENT_ROOT']);
 define("DEFCONTENT",'pages');
 define("DEFDATADIR",'data');
 define("DEFAULTEDITMODE","file");
 define("DEFPAGESESVAR",'defaultpage');
-define("DOCROOTSTRLEN", strlen($_SERVER['DOCUMENT_ROOT']));
-define("DOMAIN",$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST']);
 define("FILETOEDIT",$_SERVER['DOCUMENT_ROOT'].'/config/filetoedit.txt');
-define("JS_ROOT",$_SERVER['DOCUMENT_ROOT'].'/js');
-define("IMG_ROOT",$_SERVER['DOCUMENT_ROOT'].'/img');
 define("IS_PHP_ERR",'errOrConf');
 define("LANGUAGES", array('da' => 'Danish','en' => 'English'));
 define("LOGGEDIN",'loggedin');
-define("PAGES_ROOT",$_SERVER['DOCUMENT_ROOT'].'/pages');
 define("REDRAW_DIR",'redrawDir');
+define("REDRAW_IMG_DIR",'redrawImgDir');
 define("REDRAW_UPPERDIR",'redrawUpperDir');
 function OSGroups() {
     $users=[];
