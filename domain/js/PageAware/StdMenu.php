b5a44608b8bbd69a70a99fdc9e1f38f98bde8f4c
Benny Andersen <a.mail.user@gmail.com>
2024-02-29 23:12:10 +0100

    simplified by moving ready function to main.js and test for progs/edit/content in javascript

--- domain/js/PageAware/StdMenu.php
+++ domain/js/PageAware/StdMenu.php
@@ -1,5 +1,3 @@
-<?php
-global $pe; ?>
 <script>
     var allFuncs={};
     const defaultPage=<?="'".defaultPage()."';\n"?>
@@ -7,13 +5,7 @@ global $pe; ?>
     const isPHPErr=<?="'".IS_PHP_ERR."';\n"?>
     const loggedInIsApache=<?=($_SESSION[LOGGEDIN] == APACHE_USER ? 'true' : 'false').";\n"?>
     const redrawDir=<?="'".REDRAW_DIR."';\n"?>
+    const redrawImgDir=<?="'".REDRAW_IMG_DIR."';\n"?>
     const redrawUpperDir=<?="'".REDRAW_UPPERDIR."';\n"?>
-<?php if (implode('/',$pe) != 'progs/edit/content'): ?>
-    $(function() {
-        let dialogState=document.cookie.split('; ').find((row) => row.startsWith('dialog='))?.split('=')[1];
-        if ( dialogState == 'on')
-            allFuncs.hamDrawMenu();
-    });
-<?php endif; ?>
 </script>
 <script type='module' <?= actors\lastmRef('/jsmodules/StdMenu/main.js','src') ?>></script> 
