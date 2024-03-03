f859d2accd4838fdf381907a7dd0c001a721d7e7
Benny Andersen <a.mail.user@gmail.com>
2024-03-04 00:00:59 +0100

    document new reduced StdMenu

--- domain/data/pages/da/docs/stdmenu.php
+++ domain/data/pages/da/docs/stdmenu.php
@@ -3,71 +3,47 @@ use function actors\srclf;
 return ["<!<div class='auto80'>#html#</div>"
 ,actors\tocHeadline($func),
     <<<EOMD
+StdMenu har dialogen hvormed sitet administreres. Ordet StdMenu er ved skride - det var engang en Menu som kunne noget specielt - den er efterhånden blevet mere en filemanager dialog end en menu.
 
 EOMD,srclf('actors/StdMenu.php','namespace pages',1,'class StdMenu',8),<<< EOMD
 $srcExpl
-hamMenu() laver html'en som anbringes før den del af body elementet som bliver layoutet fra data.
+
+dialogen hæftes på div'en id med class="modal" vha javascript. På den måde fremtræder html'en kilden mere oerskuelig.  
+Ved klik eller key enter på en side fra dialogen bliver html kilden prettyfied.
 </div>
 
-### function hamMenu()
+### Overførsel af context fra php til javascript
 
-#### Handlingen der åbner dialog menuen
+EOMD,srclf('js/PageAware/StdMenu.php','script','^$'),<<< EOMD
 
-EOMD,srclf('actors/StdMenu.php','id="hammenu"',1),<<< EOMD
-$srcExpl
-En button med et hamburger unicode tegn og navnet på funktionen der åbner dialog
-</div>
-
-EOMD,srclf('js/PageAware/StdMenu.php','allFuncs',1,"type='module'",1),<<<EOMD
-
-EOMD,srclf('jsmodules/StdMenu/main.js','hamDrawMenu',1,'allFuncs.hamDrawMenu',1),
-srclf('jsmodules/StdMenu/hamMenu.js','function hamDrawMenu',1,'myModal',1),<<<EOMD
-$srcExpl
-Her vises alene den css property tildeling som synliggør dialog menuen
-</div>
-
-EOMD,srclf('actors/StdMenu.php','id="myModal"',2),<<< EOMD
-$srcExpl
-Dialog menuens omsluttende div - indeni er html der giver den indhold.
-</div>
+### Dialogens html
+EOMD,srclf('jsmodules/StdMenu/main.js','let dlgHtml','^$'),<<< EOMD
 
 #### Html tags indeni dialog menuen
 Dialog menuen er delt op tre kolonner gennem styling.
 
-#####variabler
-
-EOMD,srclf('actors/StdMenu.php','isLoggedIn',3),<<< EOMD
-$srcExpl
-variabler for tekst og url links for login/logout.
-</div>
-
-EOMD,srclf('actors/StdMenu.php','DEFAULTEDITMODE',3),<<< EOMD
-$srcExpl
-Ikon for valg af om editering skal ske i browser eller ej (blot fil ref som lokal editor tilgår)
-</div>
-
 ##### Venstre kolonne:
-EOMD,srclf('actors/StdMenu.php','span title="Home page"',2),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js','span title="Home page"',2),<<< EOMD
 $srcExpl
 De to span elementer - et link til roden og et tilbagelink.
 </div>
 
-EOMD,srclf('actors/StdMenu.php',"id='curDirStr'",1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='curDirStr'",1),<<< EOMD
 $srcExpl
 Path for current directory.
 </div>
 
-EOMD,srclf('actors/StdMenu.php',"id='wdFiles'",1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='wdFiles'",1),<<< EOMD
 $srcExpl
 ul element hvori javascript indsætter current directories filer
 </div>
 
-EOMD,srclf('actors/StdMenu.php',"id='statusLine'",1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='statusLine'",1),<<< EOMD
 $srcExpl
 Statuslinie som bruges til diverse tilbagemelding og  bla. attributter for valgte fil i dir listen
 </div>
 
-EOMD,srclf('actors/StdMenu.php','form onsubmit',5),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js','onsubmit',6),<<< EOMD
 $srcExpl
 Html form bliver synlig for input af fil eller dir navn for oprettelse eller omdøbning, og bekræftning eller afbrydelse for sletning af fil eller dir
 </div>
@@ -76,23 +52,24 @@ Html form bliver synlig for input af fil eller dir navn for oprettelse eller omd
 ##### midterste kolonne:
 
 
-EOMD,srclf('actors/StdMenu.php','a href',1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"a href",1),<<< EOMD
 $srcExpl
-Login/logout link vha variabler fra linie 6
+Login/logout link vha variabler fra js/PageAware/StdMenu.php linie 22
 </div>
 
 
-EOMD,srclf('actors/StdMenu.php','editmode:',2),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"editmode",2),<<< EOMD
 $srcExpl
-Og valg af edit måde vha. variabler fra linie 10
+Og valg af edit måde vha. variabler fra js/PageAware/StdMenu.php linie 23
 </div>
 
 ##### højre kolonne:
-EOMD,srclf('actors/StdMenu.php',"id='close'",3),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='close'",2),<<< EOMD
 $srcExpl
 Alene close button øverst
 </div>
 
-Det var lidt om html'en i function hamMenu(). Det forklarer intet om hvordan interaktiviteten er tilvejebragt, for det er javascript og AJAX. Før vi ser på det så en lille leg med noget simplere mekanik i næste kapitel.
+Det var om html'en som 'bliver levende' som følge af javascript tilføjelser, ændringer og synliggørelser i html'en. Det udløses af keys som fanges i keyhandlers og forespørger webserveren vha AJAX og PHP. 
+Først et forsøg på at forklare API på en simpel måde.  
 
 EOMD,actors\tocNavigate($func)];
--- domain/data/pages/en/docs/stdmenu.php
+++ domain/data/pages/en/docs/stdmenu.php
@@ -3,97 +3,74 @@ use function actors\srclf;
 return ["<!<div class='auto80'>#html#</div>"
 ,actors\tocHeadline($func),
     <<<EOMD
+StdMenu has the dialog with which the site is administered. The word StdMenu is on the way out - it was once a Menu that could do something special - it has gradually become more of a file manager dialog than a menu.
 
 EOMD,srclf('actors/StdMenu.php','namespace pages',1,'class StdMenu',8),<<< EOMD
 $srcExpl
 
-hamMenu() creates the html that is placed before the part of the body element that is laid out from data
+the dialog is attached to the div id with class="modal" using javascript. In this way, the html source appears more clearly.  
+When you click or key enter on a page from the dialog, the html source becomes prettyfied.
 </div>
 
-### function hamMenu()
+### Transfer of context from PHP til javascript
 
-#### The action that opens the dialog menu
+EOMD,srclf('js/PageAware/StdMenu.php','script','^$'),<<< EOMD
 
-EOMD,srclf('actors/StdMenu.php','id="hammenu"',1),<<< EOMD
-$srcExpl
-A button with a hamburger unicode character and the name of the function that opens a dialog
-</div>
+### The html source of the dialog
+EOMD,srclf('jsmodules/StdMenu/main.js','let dlgHtml','^$'),<<< EOMD
 
-EOMD,srclf('js/PageAware/StdMenu.php','allFuncs',1,"type='module'",1),<<<EOMD
-
-EOMD,srclf('jsmodules/StdMenu/main.js','hamDrawMenu',1,'allFuncs.hamDrawMenu',1),
-srclf('jsmodules/StdMenu/hamMenu.js','function hamDrawMenu',1,'myModal',1),<<<EOMD
-$srcExpl
-Only the css property assignment that makes the dialog menu visible is shown here
-</div>
-
-EOMD,srclf('actors/StdMenu.php','id="myModal"',2),<<< EOMD
-$srcExpl
-The dialog menu's enclosing div - inside is the html that gives it content.
-</div>
-
-#### Html tags inside the dialog menu
-The dialog menu is divided into three columns through styling.
-
-#####variables
-
-EOMD,srclf('actors/StdMenu.php','isLoggedIn',3),<<< EOMD
-$srcExpl
-variables for text and url links for login/logout.
-</div>
-
-EOMD,srclf('actors/StdMenu.php','DEFAULTEDITMODE',3),<<< EOMD
-$srcExpl
-Icon for choosing whether editing should take place in the browser or not (only file ref as local editor is accessed)
-</div>
+#### Html tags inside the dialog
+The dialog is splitted in three columns through styling
 
 ##### Left column:
-EOMD,srclf('actors/StdMenu.php','span title="Home page"',2),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js','span title="Home page"',2),<<< EOMD
 $srcExpl
-The two span elements - a link to the root and a backlink.
+The two span elements is a link to the root and a back link to upper directory
 </div>
 
-EOMD,srclf('actors/StdMenu.php',"id='curDirStr'",1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='curDirStr'",1),<<< EOMD
 $srcExpl
 Path for current directory.
 </div>
 
-EOMD,srclf('actors/StdMenu.php',"id='wdFiles'",1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='wdFiles'",1),<<< EOMD
 $srcExpl
-ul element in which javascript inserts the current directory's files
+ul elements i which javascript inserts current directories files
 </div>
 
-EOMD,srclf('actors/StdMenu.php',"id='statusLine'",1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='statusLine'",1),<<< EOMD
 $srcExpl
 Status line, which is used for various feedback and e.g. attributes for selected file in the dir list
 </div>
 
-EOMD,srclf('actors/StdMenu.php','form onsubmit',5),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js','onsubmit',6),<<< EOMD
 $srcExpl
 Html form becomes visible for input of file or dir name for creation or renaming, and confirmation or abort for deletion of file or dir
 </div>
 
 
-##### middle column:
+##### Middle column:
 
 
-EOMD,srclf('actors/StdMenu.php','a href',1),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"a href",1),<<< EOMD
 $srcExpl
-Login/logout link using variables from line 6
+Login/logout link using variables from js/PageAware/StdMenu.php linie 22
 </div>
 
 
-EOMD,srclf('actors/StdMenu.php','editmode:',2),<<< EOMD
+EOMD,srclf('jsmodules/StdMenu/main.js',"editmode",2),<<< EOMD
 $srcExpl
-And selection of edit mode using variables from line 10
+Choice of edit way using variables from js/PageAware/StdMenu.php linie 23
 </div>
 
-##### Right column:
-EOMD,srclf('actors/StdMenu.php',"id='close'",3),<<< EOMD
+##### Rigth  column:
+EOMD,srclf('jsmodules/StdMenu/main.js',"id='close'",2),<<< EOMD
 $srcExpl
-Only close button at top.
+Just close button at top
 </div>
 
-It was about the html in the function hamMenu(). It explains nothing about how the interactivity is provided, because that is javascript and AJAX. Before we look at this, a demo with simpler mechanics follows in the next chapter.
+It was about the html which 'becomes alive' as a result of javascript additions, changes and visibility in the html. It is triggered by keys that are captured in keyhandlers and query the web server using AJAX and PHP.  
+
+First an attempt to explain API in a simple way.
 
 EOMD,actors\tocNavigate($func)];
