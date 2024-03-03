ccf10aae40bf1070e7d2cdc35e1974d2f4f40021
Benny Andersen <a.mail.user@gmail.com>
2024-03-04 00:01:33 +0100

    Simplified StdMenu

--- domain/actors/StdMenu.php
+++ domain/actors/StdMenu.php
@@ -1,16 +1,10 @@
 <?php
 namespace actors;
 
-function dialogTopDiv() {
-    global $pe; ?>
-    <button id="hammenu" onclick="allFuncs.hamDrawMenu();">&#8801;</button><a title='prettify html' href='/?path=progs/mkPage&amp;redir=<?=implode('/',$pe)?>'>🦋</a><div id="myModal" class="modal"></div>
-<?php }
-
 class StdMenu extends PageAware {
     protected $jsFiles= ['/js/jquery.min.js'];
 
     function stdContent() {
-    	dialogTopDiv();
-        echo $this->body;
+    	echo "<div id='myModal' class='modal'></div>\n".$this->body;
 	}
 }
