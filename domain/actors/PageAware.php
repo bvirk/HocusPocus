6c2fcf23278ad3b5936b84ace9540df3b499a944
Benny Andersen <a.mail.user@gmail.com>
2024-04-01 17:24:53 +0200

    class enheritance made optionally + attributes in link and scripttags

--- domain/actors/PageAware.php
+++ domain/actors/PageAware.php
@@ -8,6 +8,7 @@ abstract class PageAware extends \HocusPocus {
     protected $title = null;
     //protected $useJSX = false;
     protected $useMarkDown=true;
+    protected $useClassInheritance=true;
     
     
     function __construct() {
@@ -36,16 +37,31 @@ abstract class PageAware extends \HocusPocus {
 
     function getExterns() {
          /** @var array $pe */ global $pe;
-        $bothPathElementsArray = ['url' => array_slice($pe,1,-1),'class' => $this->enheritPathElements()];
+        
+        $bothPathElementsArray=['url' => array_slice($pe,1,-1)];
+        if ($this->useClassInheritance)
+            $bothPathElementsArray['class'] = $this->enheritPathElements();
         
         $this->incFiles("<link rel='stylesheet' type='text/css'",'href','css','',$bothPathElementsArray);
-        foreach($this->cssFiles as $cssFile)  
-            echo "  <link rel='stylesheet' type='text/css' href='$cssFile'>\n";
-        foreach($this->jsFiles as $jsFile)
-            echo "  <script src='$jsFile'></script>\n";
+        $this->incPropDeclFiles('<link','href','',$this->cssFiles);
+        $this->incPropDeclFiles('<script', 'src','</script>',$this->jsFiles);
         $this->incFiles("<script",'src','js','</script>',$bothPathElementsArray);
     }
 
+    function incPropDeclFiles($startTag,$refAtt,$endTag,$FilesArray) {
+        foreach ($FilesArray as $fileSpec) {
+            if (!is_array($fileSpec))
+                $fileSpec = [$fileSpec];
+            $ref=array_shift($fileSpec);
+            if (str_ends_with($ref,'?'))
+                $ref .= "lastm=".filemtime(substr($ref,1,strlen($ref)-2));
+            $scriptTag = "$startTag $refAtt='$ref' ";
+            foreach ($fileSpec as $attName => $attValue)
+                $scriptTag .= "$attName='$attValue' ";
+            echo "  $scriptTag>$endTag\n";
+        }
+    }
+        
     function incFiles($tagAndOtherAttributes,$refAtt,$ext,$endTag,$pathElementsArr) { 
         /** @var array $pe */ global $pe;
         foreach ($pathElementsArr as $pType => $pathElements) {
