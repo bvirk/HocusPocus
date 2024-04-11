3a32562e359fecd1f9ea2ff31725450a02e48c43
Benny Andersen <a.mail.user@gmail.com>
2024-04-11 23:30:31 +0200

    test changes

--- domain/jsmodules/XDialog/test.js
+++ domain/jsmodules/XDialog/test.js
@@ -1,11 +1,37 @@
-export default function snip() {
-    return 'snapsnude';
+
+function setProps([obj]) {
+    for (const pr in obj)
+        this[pr] = obj[pr];
 }
 
-export function snap() {
-    return "snip";
+function stringifyObject(obj) {
+    let result = '';
+
+    for (const key in obj) {
+        if (typeof obj[key] !== 'function')
+            result += typeof obj[key] === 'object'
+                ? `${key}: {${stringifyObject(obj[key])}}, `
+                : `${key}: ${obj[key]}, `;
+    }
+    return result;
 }
 
-export function snude() {
-    return "skaft";
+function listProps() {
+    return stringifyObject(this);
 }
+
+
+function setplace() {
+    let [look,newplace] = this.place == 'file'
+        ? ['🌥 ☑','http']
+        : ['🌥 ☐','file']; 
+    $('#editplace').text(look);
+    this.place = newplace;
+    
+}
+
+
+const module = { place:"file", setplace }; 
+
+export default module;
+
