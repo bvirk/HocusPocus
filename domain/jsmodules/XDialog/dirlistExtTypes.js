feae762f3358a400d58bbfb1423e8d2bbaa24b2f
Benny Andersen <a.mail.user@gmail.com>
2024-04-11 23:16:37 +0200

    dummy cacheSelFile() for equal use as dirWebRoot.js

--- domain/jsmodules/XDialog/dirlistExtTypes.js
+++ domain/jsmodules/XDialog/dirlistExtTypes.js
@@ -3,14 +3,18 @@ let dirlist = [[
     ,['js' ,'+js' ,'js files','',0]
     ,['img','+img','image files','',0]]];
 
+
+export function cacheSelFile() {
+}
+
 export function dirlistData(drawer) {
         drawer(dirlist,true);
 }
-export function setDirty() { this.selIndex=0; return module;}
+export function setDirty() { return module;}
 
 export function typeName() { 
     return dirlist[0][this.selIndex][0]; 
 }
 
-const module = { dirIndex: 0, length: 3, selIndex: 0, dirlistData, setDirty, typeName };
+const module = { cacheSelFile, dirIndex: 0, length: 3, selIndex: 0, dirlistData, setDirty, typeName };
 export default module;
