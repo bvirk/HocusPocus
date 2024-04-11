cbd47e3b555a28ee99b718f42fb0bb508cb60535
Benny Andersen <a.mail.user@gmail.com>
2024-04-11 23:28:09 +0200

    invoke help

--- domain/jsmodules/XDialog/keysWebRoot.js
+++ domain/jsmodules/XDialog/keysWebRoot.js
@@ -1,8 +1,9 @@
 import * as view from './dirView.js';
 import * as fm from './filemanage.js';
 import {getRequest} from './requests.js';
-import {APIName,SES } from './webPageContext.js'
+import conf, { APIName } from './webPageContext.js'
 import dirWR from "./dirlistWebRoot.js";
+import { KeyHandler, setCurkeyhandler } from './keyHandlerDelegater.js';
 
 export function whenWebRoot(event) {
     if (event.defaultPrevented)
@@ -26,11 +27,18 @@ export function whenWebRoot(event) {
             view.selectAbove();
             break;
         case "a":
-            view.statusLine(SES.editmode);
+            view.statusLine(conf.listProps());
+            break;
+        case "b":
+            
             break;
         case "e":
             let file = dirWR.selFileItem()[0];
             getRequest(fm.edit,APIName+'edit&filetoedit='+file);
+            break;
+        case "h":
+            getRequest(fm.webRootHelp,'/?path=progs/NNNAPI/help&type=nav');
+            setCurkeyhandler(KeyHandler.HELP,KeyHandler.WEBROOT);
             break;
         case "q":
         case "Escape":
@@ -39,6 +47,3 @@ export function whenWebRoot(event) {
     }
     //event.preventDefault();
 }
-
-
-
