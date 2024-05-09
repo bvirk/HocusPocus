0992a8eb3461cbaba8eeabf54185be85da42b113
Benny Andersen <a.mail.user@gmail.com>
2024-05-09 22:36:22 +0200

    exception now thows javscript object

--- domain/index.php
+++ domain/index.php
@@ -67,8 +67,10 @@ function instantiatePath() {
                 errLog($e);
                 [$file,$line]=[$e->getfile(),$e->getLine()];
                 file_put_contents(FILETOEDIT,str_replace(' ','_',$e->getMessage())." $file:$line");
-                echo json_encode([IS_PHP_ERR,"PHP ERROR in .../".substr($file,strlen($_SERVER['DOCUMENT_ROOT'])+1).":$line"]);
-            } else {
+                $errMes = "PHP ERROR in .../".substr($file,strlen($_SERVER['DOCUMENT_ROOT'])+1).":$line";
+                //echo json_encode([IS_PHP_ERR,$errMes]);
+                echo json_encode([PHP_ERR => $errMes,'message' => $e->getMessage()]);
+            } else  {
                 headerCTText();    
                 echo "$e\n";
             }
