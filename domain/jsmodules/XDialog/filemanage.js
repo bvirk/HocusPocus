262a96dd27e8e5108ab90a122c28ed9a99516d00
Benny Andersen <a.mail.user@gmail.com>
2024-05-09 22:59:13 +0200

    linux like dir functions

--- domain/jsmodules/XDialog/filemanage.js
+++ domain/jsmodules/XDialog/filemanage.js
@@ -1,15 +1,34 @@
 import * as view from './dirView.js';
 
-export function edit([file,editLoc]) {
+export function edit({file,editLoc}) {
     view.statusLine(file+' served as '+editLoc);
-    if (editLoc == 'http' && (view.dirlist[view.selIndex][4] & 1)) {
+    if (editLoc == 'http' && (view.dir.selFileItem().filePermstat & 1)) {
         window.open('/progs/edit/content','_blank');
     }
     
 }
 
-export function webRootHelp([unused,resp]) {
+
+export function basename(path) {
+    return path.split('/').reverse()[0];
+}
+
+export function barename(fileName) {
+    let f= basename(fileName);
+    return f.substring(0,f.lastIndexOf('.'));
+}
+
+export function dirname(path) {
+    return path.split('/').slice(0,-1).join('/');
+}
+
+export function extension(fileName) {
+    let f= basename(fileName);
+    return f.substring(f.lastIndexOf('.')+1);
+}
+
+export function webRootHelp({content}) {
     $('#dialog').css('display','none');
-    $('#dialog-help').html(resp).css('display','block');
+    $('#dialog-help').html(content).css('display','block');
 
 }
