c9785b2a83c90ad4f11b105866fb3405ec211ddf
Benny Andersen <a.mail.user@gmail.com>
2024-05-09 22:52:03 +0200

    use and config in one function for login/logout and flag for editing in browser

--- domain/jsmodules/XDialog/buttons.js
+++ domain/jsmodules/XDialog/buttons.js
@@ -1,6 +1,9 @@
 import conf from './webPageContext.js'
-import { getRequest } from './requests.js';
-import { APIName, IS_PHP_ERR } from './webPageContext.js';
+import { postRequest } from './requests.js';
+import { APIClass, PHP_ERR } from './webPageContext.js';
+import { showLoginInput } from './formsubmit.js';
+import { cdhome, fetchWebRoot, statusLine, wRPath } from './dirView.js';
+
 
 /*
 add events and look to buttons
@@ -15,46 +18,60 @@ export function attachClick(id,callback) {
 
 
 
-function setupEditModeButton() {
-    document.getElementById('editplace').addEventListener('click', () => {
-        let newplace = conf.SES.editmode == 'file' ? 'http' : 'file';
-        getRequest(setEditmode,APIName+'setSessionVar&sessionvar=editmode&editmode='+newplace);
-    });
-    setEditmode([0,conf.SES.editmode]);
+function attButEditmode(obj) {
+    if (obj === undefined) {
+        document.getElementById('editplace').addEventListener('click', () => {
+            postRequest(
+                 attButEditmode.bind(this)
+                ,{sessionvar:'editmode',editmode:this.editmode == 'file' ? 'http' : 'file'}
+                ,APIClass+'setSessionVar');
+        });
+    } else 
+        this.editmode=obj['editmode'];
+    let [title,look] = this.editmode == 'file' 
+        ? ['click to edit online','🌥 ☐']
+        : ['click to edit local','🌥 ☑'];
+    $('#editplace').text(look).attr('title',title);
+        
 }
 
-function setEditmode([err,value]) {
-    let myVal = value;
-    if (err === IS_PHP_ERR)
-        statusLine(value);
-    else {
-        conf.SES.editmode=value;
-        let [title,look] = conf.SES.editmode == 'file' 
-            ? ['click to edit online','🌥 ☐']
-            : ['click to edit local','🌥 ☑'];
-        $('#editplace').text(look).attr('title',title);    
+
+
+export function attLoginButton(obj) {
+    if (obj === undefined) {
+        document.getElementById('login').addEventListener('click', () => {
+            if (conf.SES.loggedin.length) {
+                if (conf.APACHE_USER === conf.SES.loggedin && wRPath.substring(0,10) !== 'data/pages')
+                    cdhome();
+                postRequest(
+                    attLoginButton.bind(this)
+                   ,{sessionvar:'loggedin',loggedin:''}
+                   ,APIClass+'setSessionVar');
+            } else 
+                showLoginInput();    
+        });
+    } else {
+        conf.SES.loggedin = obj['loggedin'];
+        if (conf.SES.loggedin === '' && 'status' in obj)
+            statusLine({login:obj['status']});
     }
-} 
-
-
-
-
-function setupLoginButton() {
-    document.getElementById('login').addEventListener('click', () =>loginLogout());
-
     let [loginTitle,loginText] = conf.SES.loggedin.length
         ? ['log out '+conf.SES.loggedin,'🔒']
         : ['log in','🔓'];
     $('#login').attr('title',loginTitle).text(loginText);
     $('#loggedin').html('&nbsp;'+conf.SES.loggedin);
+    fetchWebRoot(obj === undefined ? true : undefined);
 }
 
-function loginLogout() {
-    let url=conf.SES.loggedin.length
-        ? '/?path=progs/loginRecieve/logout&url='+window.location.pathname
-        : '/?path=progs/html/login&url='+window.location.pathname;
-    window.location = url;
+export function loginSignup(reset=false) {
+    let [butlook,formh3,submit] = reset || $('#loginsignup').text() === 'Login'
+        ? ['Sign up','Login','Login']
+        : ['Login','Make Encryption of password','Save encryption'];
+        $('#loginsignup').text(butlook);
+        $('#loginform h3').text(formh3);
+        $('#loginform input[type="submit"]').attr('value',submit);
+
 }
 
-const module = {setupEditModeButton,setupLoginButton}
+const module = {attButEditmode,attLoginButton,editmode:'file'}
 export default module;
