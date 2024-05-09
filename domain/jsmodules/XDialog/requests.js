a0d71ebb7df733408dbf08a2ab8b850e204c3dc5
Benny Andersen <a.mail.user@gmail.com>
2024-05-09 23:10:57 +0200

    postrequest takes all and apiAnswer for apifunction dependt progress

--- domain/jsmodules/XDialog/requests.js
+++ domain/jsmodules/XDialog/requests.js
@@ -1,16 +1,51 @@
-import {IS_PHP_ERR } from './webPageContext.js'
+import {fetchDir, statusLine} from './dirView.js';
+import {PHP_ERR } from './webPageContext.js'
 
-export async function getRequest(reciever,url) {
-    const response = await fetch(url);
-    const text = await response.text();
-    try {
-        //console.log('trying in fether');
-        reciever(JSON.parse(text),false); // which not cathes - it will happend here.
-    } catch(e) {
-        reciever([IS_PHP_ERR,e.message.startsWith('JSON.parse')
-            ? text
-            : `${e.name}: ${e.message} at ${e.fileName}:${e.lineNumber}` 
-        ],false);
+export function apiAnswer(resp) {
+    let mes='';
+    let apiFunc = Object.keys(resp)[0];
+    switch(apiFunc) {
+        case 'mvImg': {
+            fetchDir();
+            break;
+        }
+        default:
+            mes = apiFunc+' not implemented';
     }
+    statusLine(mes ? mes : JSON.stringify(resp));
 }
 
+export async function postRequest(reciever,data,url) {
+    const formData = new URLSearchParams();
+    
+    for (const prop in data)
+        formData.append(prop,data[prop]);     
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formData.toString()
+    };
+
+    const response = await fetch(url, options);
+    const text = await response.text();
+
+    try {
+        if (!text.length) {
+            statusLine('empty response - had php Echoed?');
+            return;
+        }
+
+        let resp=JSON.parse(text);
+        if (PHP_ERR in resp)
+            statusLine(resp);
+        else
+            reciever(resp,false); // which not cathes - it will happend here.
+    } catch(e) {
+        statusLine(e.message.startsWith('JSON.parse')
+        ? text 
+        : `${e.name}: ${e.message} at ${e.fileName}:${e.lineNumber}`);
+    }
+}
+  
