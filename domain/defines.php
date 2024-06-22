26a26ee484fbc72963917b95a5d20ae3470b1090
Benny Andersen <a.mail.user@gmail.com>
2024-06-22 22:47:34 +0200

    spaces added

--- domain/defines.php
+++ domain/defines.php
@@ -1,19 +1,19 @@
 <?php
-define("APACHE_USER",exec('whoami'));
-define("AUTHFILE","config/encrypted.php");
-define("CONFIRM_COMMAND",'errOrConf');
-define("DEFCONTENT",'pages');
-define("DEFDATADIR",'data');
-define("DEFAULTEDITMODE","file");
-define("DEFPAGESESVAR",'defaultpage');
-define("FILETOEDIT",$_SERVER['DOCUMENT_ROOT'].'/config/filetoedit.txt');
-define("IS_PHP_ERR",'errOrConf');
-define("LANGUAGES", array('da' => 'Danish','en' => 'English'));
-define("LOGGEDIN",'loggedin');
-define("PHP_ERR",'phpError');
-define("REDRAW_DIR",'redrawDir');
-define("REDRAW_IMG_DIR",'redrawImgDir');
-define("REDRAW_UPPERDIR",'redrawUpperDir');
+define( "APACHE_USER", exec('whoami') );
+define( "AUTHFILE", "config/encrypted.php" );
+define( "CONFIRM_COMMAND", 'errOrConf' );
+define( "DEFCONTENT", 'pages' );
+define( "DEFDATADIR", 'data' );
+define( "DEFAULTEDITMODE", "file" );
+define( "DEFPAGESESVAR", 'defaultpage' );
+define( "FILETOEDIT", $_SERVER['DOCUMENT_ROOT'].'/config/filetoedit.txt' );
+define( "IS_PHP_ERR", 'errOrConf' );
+define( "LANGUAGES", array('da' => 'Danish','en' => 'English') );
+define( "LOGGEDIN", 'loggedin' );
+define( "PHP_ERR", 'phpError' );
+define( "REDRAW_DIR", 'redrawDir' );
+define( "REDRAW_IMG_DIR", 'redrawImgDir' );
+define( "REDRAW_UPPERDIR", 'redrawUpperDir' );
 function OSGroups() {
     $users=[];
     foreach (file('/etc/passwd') as $user)  {
