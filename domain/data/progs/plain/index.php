d359fb986de59d629950d7d1f45beff7a5c3c9e4
Benny Andersen <a.mail.user@gmail.com>
2024-06-22 22:40:55 +0200

    testing function logvars()

---
+++ domain/data/progs/plain/index.php
@@ -0,0 +1,9 @@
+<?php
+$some='kurtistan';
+$tmpdir = sys_get_temp_dir();
+\actors\logvars(get_defined_vars());
+
+return[ <<< EOMD
+TESTING SOME AWESOME THINGS
+I am glad to meat you found $tmpdir
+EOMD];
